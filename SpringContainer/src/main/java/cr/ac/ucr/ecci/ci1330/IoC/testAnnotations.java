8f39bc62e2c5d9ed4250eb1e15c83dd97df2417b
alexiabl <a1b9l9d6>
2017-09-28 23:46:18 -0600

    parser update

--- SpringContainer/src/main/java/cr/ac/ucr/ecci/ci1330/IoC/testAnnotations.java
+++ SpringContainer/src/main/java/cr/ac/ucr/ecci/ci1330/IoC/testAnnotations.java
@@ -1,7 +1,10 @@
 package cr.ac.ucr.ecci.ci1330.IoC;
 
 import cr.ac.ucr.ecci.ci1330.IoC.AnnotationBased.AnnotationBeanFactory;
+import cr.ac.ucr.ecci.ci1330.IoC.annotadedClasses.Mascota;
+import cr.ac.ucr.ecci.ci1330.IoC.annotadedClasses.Persona;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -21,4 +24,10 @@ public class testAnnotations {
         }
         beanFactory.getBean(annotatedClasses.get(0).getSimpleName());
     }
+/*    List<Class> classesToScan = new LinkedList<>();
+        classesToScan.add(Persona.class);
+        classesToScan.add(Mascota.class);
+    testAnnotations testAnnotations = new testAnnotations(classesToScan);
+        testAnnotations.executeTest();*/
+
 }

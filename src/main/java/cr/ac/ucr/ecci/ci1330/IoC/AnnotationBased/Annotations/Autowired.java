a197accdd310d92dd709f061f4c4b1fa750967ee
alexiabl <a1b9l9d6>
2017-09-30 01:28:44 -0600

    Annotations documentacion html

--- src/main/java/cr/ac/ucr/ecci/ci1330/IoC/AnnotationBased/Annotations/Autowired.java
+++ src/main/java/cr/ac/ucr/ecci/ci1330/IoC/AnnotationBased/Annotations/Autowired.java
@@ -2,12 +2,15 @@ package cr.ac.ucr.ecci.ci1330.IoC.AnnotationBased.Annotations;
 
 import cr.ac.ucr.ecci.ci1330.IoC.AutowiringMode;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Created by alexiaborchgrevink on 9/24/17.
  */
+@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Autowired {
 

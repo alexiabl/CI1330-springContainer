282647256254e07612e27e023f8a40a2e2669ce4
majocubero <majo.qbro@gmail.com>
2017-09-30 21:59:14 -0600

    borrando clases innecesarias

--- src/main/java/cr/ac/ucr/ecci/ci1330/IoC/Main.java
+++ src/main/java/cr/ac/ucr/ecci/ci1330/IoC/Main.java
@@ -2,46 +2,10 @@ package cr.ac.ucr.ecci.ci1330.IoC;
 
 import cr.ac.ucr.ecci.ci1330.IoC.AnnotationBased.AnnotationBeanFactory;
 import cr.ac.ucr.ecci.ci1330.IoC.XMLBased.XMLBeanFactory;
-import cr.ac.ucr.ecci.ci1330.IoC.annotadedClasses.Mascota;
-import cr.ac.ucr.ecci.ci1330.IoC.annotadedClasses.Persona;
-import cr.ac.ucr.ecci.ci1330.IoC.AnnotationBased.testAnnotations;
-
-import java.util.LinkedList;
-import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
-        AbstractBeanFactory annotationTest = new AnnotationBeanFactory("CI1330-springContainer/SpringContainer/src/main/resources/xmlAnnotations.xml");
-        Persona persona = (Persona) annotationTest.getBean("Persona");
-        String path = "SpringContainer/src/main/resources/beans.xml";
-        AbstractBeanFactory abstractBeanFactory= new XMLBeanFactory(path);
-
-        System.out.println("beanHashMap.size()= "+abstractBeanFactory.beanHashMap.size());
-
-      //  Persona persona= (Persona) abstractBeanFactory.getBean("Persona");
-        //System.out.println(persona.getNombre());
-
-        testeReflection testeReflection= (cr.ac.ucr.ecci.ci1330.IoC.testeReflection) abstractBeanFactory.getBean("teste");
-
-       // System.out.println(testeReflection.getMascota().getName());
-        //System.out.println(testeReflection.getEstudiante().getName());
-
-       // System.out.println();
-     /*   System.out.println(testeReflection.getEst().getName());
-        System.out.println(testeReflection.getEstudiante().getName());
-        System.out.println(testeReflection.getMascota().getName());
-      //  System.out.println(testeReflection.);
-/*
-        testeReflection.setNum(9);
-        System.out.println(testeReflection.getP().getName());
-        testeReflection testeReflection2= (cr.ac.ucr.ecci.ci1330.IoC.testeReflection) abstractBeanFactory.getBean("teste");
-        System.out.println(testeReflection2.getNum());
-        testeReflection.setNum(3);
-        System.out.println(testeReflection.getNum());
-*/
-        //errores: al constructor no se le puede poner mas de un parametro
-
 
     }
 }
